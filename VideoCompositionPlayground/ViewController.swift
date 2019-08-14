4d4579b38de4ad6a955429a0c19f5e1d6e66d4ad
Reynaldo Aguilar <reynaldo@canva.com>
2019-08-14 15:37:41 +1000

    Initial Commit

---
+++ VideoCompositionPlayground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoCompositionPlayground
+//
+//  Created by Reynaldo Aguilar on 14/8/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
